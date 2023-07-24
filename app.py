14caf85bb4f1d4055efa79603e70ad08a19d7952
saten-private <38902911+saten-private@users.noreply.github.com>
2023-07-24 19:45:44 +0900

    Unspecified background image

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     UNLOAD_MODEL = args.unload_model
 
-    predict("melody", "", None, 300, 250, 0, 1.0, 3.0, -1)
+    predict("melody", "", None, 300, 250, 0, 1.0, 3.0, -1, 5, False, "")
     # ui(
     #     username=args.username,
     #     password=args.password,
